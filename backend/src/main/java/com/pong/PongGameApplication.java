c968292a4f2dcf18d93e87575b0c18c9bb3ab69d
Ali Burak Bozan <jeagle_1907@hotmail.com>
2026-03-23 23:49:40 +0300

    Create PongGameApplication class in backend/src/main/java/com/pong

---
+++ backend/src/main/java/com/pong/PongGameApplication.java
@@ -0,0 +1,12 @@
+package com.pong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PongGameApplication.class, args);
+    }
+}
