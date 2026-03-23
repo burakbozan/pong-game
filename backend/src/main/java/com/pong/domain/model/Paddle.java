054e0ee71a6d489134ee5c25db64892b363b9f62
Ali Burak Bozan <jeagle_1907@hotmail.com>
2026-03-23 23:49:00 +0300

    Create Paddle.java as a domain entity for paddles

---
+++ backend/src/main/java/com/pong/domain/model/Paddle.java
@@ -0,0 +1,49 @@
+// Paddle.java
+
+package com.pong.domain.model;
+
+public class Paddle {
+    private double positionX;
+    private double positionY;
+    private double width;
+    private double height;
+
+    public Paddle(double positionX, double positionY, double width, double height) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(double positionX) {
+        this.positionX = positionX;
+    }
+
+    public double getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(double positionY) {
+        this.positionY = positionY;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
