6da9ac27a2d31095d01087ac3f8dba8f17962523
Ali Burak Bozan <jeagle_1907@hotmail.com>
2026-03-23 23:44:32 +0300

    Create App.jsx for React Pong game component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Pong Game</h1>
+      {/* Game component goes here */}
+    </div>
+  );
+}
+
+export default App;
