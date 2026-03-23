afda9b5b9156e0da4394dc01dfa679eb85812774
Ali Burak Bozan <jeagle_1907@hotmail.com>
2026-03-23 23:45:50 +0300

    Create GameCanvas.jsx for rendering the Pong game

---
+++ frontend/src/components/GameCanvas.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GameCanvas = () => {
+    return (
+        <canvas id="pong" width="800" height="400">
+            Your browser does not support the canvas element.
+        </canvas>
+    );
+};
+
+export default GameCanvas;
